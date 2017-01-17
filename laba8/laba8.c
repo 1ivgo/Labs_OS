f5121aaefa6dcc2e182f5ec56d024a1923c0d5ef
1ivgo <ivgo1@yandex.ru>
2017-01-17 15:20:37 +0300

    Fixed output data

--- laba8/laba8.c
+++ laba8/laba8.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
-#include <string.h>
-#include  <fcntl.h>
-#include <time.h>
-#include <sys/time.h>
-#include <semaphore.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
 #include <unistd.h>
+#include <time.h>
+#include <string.h>
 
 pthread_cond_t cond_var = PTHREAD_COND_INITIALIZER;
 pthread_mutex_t mutex;
@@ -20,29 +21,27 @@ void* thread_func_writer(void * arg){
 	int i, try_lock, fd;
 	int len_str;
 	
-	while(num_str<10){
-		
-		pthread_mutex_lock(&mutex);
-
-		if ((fd = open("./File.txt", O_RDWR | O_CREAT, 0666)) == -1){
+	if ((fd = open("./File.txt", O_RDWR | O_CREAT, 0744)) == -1){
 			printf("Ошибка открытия файла\n");
 			exit(0);
-		}
-		
+	}
+
+	while(1){
+		pthread_mutex_lock(&mutex);
+
 		cur_time = time(NULL);
 		tv = localtime(&cur_time);
 		len_str = sprintf (str, "%d\t %s", num_str, asctime(tv));
 		write(fd, str, len_str);
-		num_str++;
 		
 		flag = 1;
+		num_str++;
+		
 		pthread_cond_signal(&cond_var);
 		pthread_mutex_unlock(&mutex);
-
 		sleep(1);
-		close(fd);
-		
 	}
+	close(fd);
 	pthread_exit(0);
 }
 
@@ -51,30 +50,28 @@ void* thread_func_reader(void* arg){
 	int fd, buffer_read=0, buffer_written=0, i;
 	char buffer[30];
 	
-	for(i=0; i<10; i++){
-
+	if ((fd = open("./File.txt", O_CREAT|O_RDONLY, 0744)) == -1){
+		printf("Ошибка открытия файла\n");
+		exit(0);
+	}	
+	while(1){
 		pthread_mutex_lock(&mutex);
 		while (flag != 1){ 
-      		pthread_cond_wait(&cond_var, &mutex);
-      	}
+	    	pthread_cond_wait(&cond_var, &mutex);
+	    }
 
-		if ((fd = open("./File.txt", O_CREAT|O_RDONLY, 0666)) == -1){
-			printf("Ошибка открытия файла\n");
-			exit(0);
-		}
-		
+		printf("Thread: %lx:\n", pthread_self());
+
 		while ((buffer_read = read(fd, buffer, 30)) > 0){
-			buffer_written = write (1, buffer, buffer_read);
+			buffer_written = write(1, buffer, buffer_read);
 			if (buffer_written != buffer_read)
 				printf("Ошибка чтения");
 		}
-
+		flag = 0;
 		pthread_mutex_unlock(&mutex);
-
-		sleep(1);
-		close(fd);
+		sleep(5);
 	}
-	flag = 0;
+	close(fd);
 	pthread_exit(0);
 }
 
@@ -86,8 +83,6 @@ int main()
 	pthread_t thread_writer;
 	pthread_t thread_reader[10];
 	
-
-
 	if(pthread_create(&thread_writer, NULL, thread_func_writer, NULL)<0)
 		printf("Ошибка создания писателя\n");
 	
